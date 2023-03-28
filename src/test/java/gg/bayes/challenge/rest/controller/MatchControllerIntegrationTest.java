aa04bc12353d3ab91614ca37e654776271ca523d
Nimish Singh <nimishoc@gmail.com>
2023-03-28 19:48:24 +0530

    Add integration test

--- src/test/java/gg/bayes/challenge/rest/controller/MatchControllerIntegrationTest.java
+++ src/test/java/gg/bayes/challenge/rest/controller/MatchControllerIntegrationTest.java
@@ -10,11 +10,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 /*
@@ -43,11 +44,97 @@ class MatchControllerIntegrationTest {
                 COMBATLOG_FILE_2, ingestMatch(COMBATLOG_FILE_2));
     }
 
-    // TODO: add your tests
-    // Replace this test method with the tests that you consider appropriate to test your implementation.
     @Test
-    void someTest() {
-        assertThat(mvc).isNotNull();
+    void shouldReturnListOfHeroesAndTheirKills() throws Exception {
+        mvc.perform(get("/api/match/{matchId}", matchIds.get(COMBATLOG_FILE_1)))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[{\"hero\":\"abyssal_underlord\",\"kills\":6},{\"hero\":\"bane\",\"kills\":2},{\"hero\":\"bloodseeker\",\"kills\":11},{\"hero\":\"death_prophet\",\"kills\":9},{\"hero\":\"dragon_knight\",\"kills\":3},{\"hero\":\"mars\",\"kills\":6},{\"hero\":\"pangolier\",\"kills\":5},{\"hero\":\"puck\",\"kills\":7},{\"hero\":\"rubick\",\"kills\":4},{\"hero\":\"snapfire\",\"kills\":2}]"));
+
+        mvc.perform(get("/api/match/{matchId}", matchIds.get(COMBATLOG_FILE_2)))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[{\"hero\":\"centaur\",\"kills\":1},{\"hero\":\"earthshaker\",\"kills\":2},{\"hero\":\"ember_spirit\",\"kills\":14},{\"hero\":\"grimstroke\",\"kills\":4},{\"hero\":\"gyrocopter\",\"kills\":3},{\"hero\":\"lycan\",\"kills\":2},{\"hero\":\"mars\",\"kills\":3},{\"hero\":\"monkey_king\",\"kills\":7},{\"hero\":\"rubick\",\"kills\":3}]"));
+    }
+
+    @Test
+    void shouldReturnEmptyListOfHeroesAndTheirKillsForNonExistentMatch() throws Exception {
+        mvc.perform(get("/api/match/{matchId}", 100))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void shouldReturnListOfItemsPurchased() throws Exception {
+        mvc.perform(get("/api/match/{matchId}/{heroName}/items", matchIds.get(COMBATLOG_FILE_1), "abyssal_underlord"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(30));
+
+        mvc.perform(get("/api/match/{matchId}/{heroName}/items", matchIds.get(COMBATLOG_FILE_2), "centaur"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(24));
+    }
+
+    @Test
+    void shouldReturnEmptyListOfItemsPurchasedForNonExistentMatch() throws Exception {
+        mvc.perform(get("/api/match/{matchId}/{heroName}/items", 100, "centaur"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void shouldReturnEmptyListOfItemsPurchasedForNonExistentHero() throws Exception {
+        mvc.perform(get("/api/match/{matchId}/{heroName}/items", matchIds.get(COMBATLOG_FILE_1), "nonExistentHero"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void shouldReturnListOfSpellsCast() throws Exception {
+        mvc.perform(get("/api/match/{matchId}/{heroName}/spells", matchIds.get(COMBATLOG_FILE_1), "abyssal_underlord"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[{\"spell\":\"abyssal_underlord_cancel_dark_rift\",\"casts\":1},{\"spell\":\"abyssal_underlord_dark_rift\",\"casts\":3},{\"spell\":\"abyssal_underlord_firestorm\",\"casts\":67},{\"spell\":\"abyssal_underlord_pit_of_malice\",\"casts\":14}]"));
+
+        mvc.perform(get("/api/match/{matchId}/{heroName}/spells", matchIds.get(COMBATLOG_FILE_2), "centaur"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[{\"spell\":\"centaur_double_edge\",\"casts\":27},{\"spell\":\"centaur_hoof_stomp\",\"casts\":8},{\"spell\":\"centaur_return\",\"casts\":4},{\"spell\":\"centaur_stampede\",\"casts\":5}]"));
+    }
+
+    @Test
+    void shouldReturnEmptyListOfSpellsCastForNonExistentMatch() throws Exception {
+        mvc.perform(get("/api/match/{matchId}/{heroName}/spells", 100, "centaur"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void shouldReturnEmptyListOfSpellsCastForNonExistentHero() throws Exception {
+        mvc.perform(get("/api/match/{matchId}/{heroName}/spells", matchIds.get(COMBATLOG_FILE_1), "nonExistentHero"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void shouldReturnListOfHeroDamages() throws Exception {
+        mvc.perform(get("/api/match/{matchId}/{heroName}/damage", matchIds.get(COMBATLOG_FILE_1), "abyssal_underlord"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[{\"target\":\"bane\",\"damage_instances\":68,\"total_damage\":3483},{\"target\":\"bloodseeker\",\"damage_instances\":196,\"total_damage\":6172},{\"target\":\"death_prophet\",\"damage_instances\":76,\"total_damage\":5865},{\"target\":\"mars\",\"damage_instances\":22,\"total_damage\":1450},{\"target\":\"rubick\",\"damage_instances\":28,\"total_damage\":1690}]"));
+
+        mvc.perform(get("/api/match/{matchId}/{heroName}/damage", matchIds.get(COMBATLOG_FILE_2), "centaur"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[{\"target\":\"centaur\",\"damage_instances\":27,\"total_damage\":7015},{\"target\":\"ember_spirit\",\"damage_instances\":32,\"total_damage\":1520},{\"target\":\"grimstroke\",\"damage_instances\":8,\"total_damage\":1187},{\"target\":\"lycan\",\"damage_instances\":27,\"total_damage\":1578},{\"target\":\"mars\",\"damage_instances\":18,\"total_damage\":1487}]"));
+    }
+
+    @Test
+    void shouldReturnEmptyListOfHeroDamagesForNonExistentMatch() throws Exception {
+        mvc.perform(get("/api/match/{matchId}/{heroName}/damage", 100, "centaur"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[]"));
+    }
+
+    @Test
+    void shouldReturnEmptyListOfHeroDamagesForNonExistentHero() throws Exception {
+        mvc.perform(get("/api/match/{matchId}/{heroName}/damage", matchIds.get(COMBATLOG_FILE_1), "nonExistentHero"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().string("[]"));
     }
 
     /**
